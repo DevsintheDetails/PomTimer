16df57ef3b0b1ad90e8bbbe1a74deaa6b5fa1058
Cody Parker <parker.cody112@gmail.com>
2021-08-02 13:04:21 -0400

    first commit

---
+++ src/pomodoro/TimerControl.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function TimerControl({
+    sessionType,
+    handleDecrease,
+    handleIncrease,
+    disabled,
+  }) {
+    return (
+      <div className="input-group-append">
+        <button
+          type="button"
+          className="btn btn-secondary"
+          data-testid={"decrease-" + sessionType}
+          onClick={handleDecrease}
+          disabled={disabled}
+        >
+          <span className="oi oi-minus" />
+        </button>
+        <button
+          type="button"
+          className="btn btn-secondary"
+          data-testid={"increase-" + sessionType}
+          onClick={handleIncrease}
+          disabled={disabled}
+        >
+          <span className="oi oi-plus" />
+        </button>
+      </div>
+    );
+  }
+  
+
+export default TimerControl;
